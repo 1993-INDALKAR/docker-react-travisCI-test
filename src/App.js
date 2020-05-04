e8741629be349629a251de49d0c89c50de6144cd
1993-INDALKAR <harishindalkar@yahoo.com>
2020-05-04 00:27:11 -0400

    removed alias name

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from  Harish Indalkar
+          Text was changed on feature branch
         </a>
       </header>
     </div>
