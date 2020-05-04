9bd8c61e2e6dddcf0a4cb6e1bce1e1bf6d11ea6b
1993-INDALKAR <harishindalkar@yahoo.com>
2020-05-04 01:54:35 -0400

    aws fix v1

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from  Harish Indalkar
+          Learn React from  Harish Indalkar changed from feature branch
         </a>
       </header>
     </div>
